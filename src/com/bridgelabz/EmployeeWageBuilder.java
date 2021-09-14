46a9d366005da6c3f80276e5434526b3f103588e
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-09-14 21:33:00 +0530

    Added: Checking Employee Attendence

--- src/com/bridgelabz/EmployeeWageBuilder.java
+++ src/com/bridgelabz/EmployeeWageBuilder.java
@@ -1,8 +1,26 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
+/**
+ * @author Utkarsh Mishra
+ * Purpose - To check whether Employee is present or absent
+ */
 public class EmployeeWageBuilder {
 
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation Program");
+        // Declaring a constant
+        int IS_FULLTIME = 1;
+        /**
+         * Using Math.random method to generate random numbers 0 and 1
+         */
+        Random random = new Random();
+        int empCheck = random.nextInt(2);
+        if (empCheck == IS_FULLTIME)
+            System.out.println("The Employee is Present");
+        else
+            System.out.println("The Employee is Absent");
+
     }
 }
