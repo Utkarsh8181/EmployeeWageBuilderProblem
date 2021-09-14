cc3ce0e630b350be72a42a6b82597968d844e722
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-09-14 22:26:22 +0530

    Added: Employee Wages according to Max Hours and Total Working Days

--- src/com/bridgelabz/EmployeeWageBuilder.java
+++ src/com/bridgelabz/EmployeeWageBuilder.java
@@ -3,7 +3,7 @@ package com.bridgelabz;
 import java.util.Random;
 /*
  * @author Utkarsh Mishra
- * Purpose - Calculating wages for a month assuming 20 working days in a month
+ * Purpose - Calculating wages for a total of working hours of 100 or max days of 20 in a month
  */
 public class EmployeeWageBuilder {
     // Declaring a constant
@@ -11,6 +11,7 @@ public class EmployeeWageBuilder {
     public static final int IS_PARTTIME = 2;
     public static final int Emp_Rate_PerHr = 20;
     public static final int NO_WORKINGDAYS = 20;
+    public static final int MAX_HRS_MONTH = 100;
 
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation Program");
@@ -18,10 +19,13 @@ public class EmployeeWageBuilder {
         int empHrs = 0;
         int empWage = 0;
         int totalEmpWage = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
         /*
          * Using random method to generate random numbers 0, 1 and 2
          */
-        for (int day = 0; day < NO_WORKINGDAYS; day++) {
+        while (totalEmpHrs <= MAX_HRS_MONTH && totalWorkingDays < NO_WORKINGDAYS) {
+            totalWorkingDays++;
             Random random = new Random();
             int empCheck = random.nextInt(3);
             switch (empCheck) {
@@ -34,13 +38,12 @@ public class EmployeeWageBuilder {
                 default: // Employee is absent
                     empHrs = 0;
             }
-            /*
-             * Calculating the daily employee wage of an employee
-             */
             empWage = Emp_Rate_PerHr * empHrs;
-            totalEmpWage += empWage;
-            System.out.println("The daily wage of an employee is : " + empWage);
+            totalEmpHrs += empHrs;
+            System.out.println("Day : " + totalWorkingDays + " : Employee worked : " + empHrs + " Hours " +
+                               ", Employee Wage : " + empWage);
         }
-        System.out.println("The total wage of an employee in a month is : " + totalEmpWage);
+        totalEmpWage = totalEmpHrs * Emp_Rate_PerHr;
+        System.out.println("Total Wages of an Employee is : " + totalEmpWage);
     }
 }
